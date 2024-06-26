1ad86fcd7bdcc6aa87585cc590e3661f430f506e
tkimanh <tran.kimanh@orange.fr>
2024-06-26 19:05:16 +0200

    Ajout des fonctions de create, findall, find, udate et remove pour la categorie

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoryEntity } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    console.log(CreateCategoryDto);
+    try {
+      const category = this.categoryRepository.create(CreateCategoryDto);
+      return this.categoryRepository.save(category);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findAll() {
-    return `This action returns all category`;
+    const categoryList = this.categoryRepository
+      .createQueryBuilder('category')
+      .getMany();
+    return categoryList;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    try {
+      const category = this.categoryRepository
+        .createQueryBuilder('category')
+        .where('category.id = :id', { id: id })
+        .getOne();
+
+      return category;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    try {
+      const category = this.categoryRepository
+        .createQueryBuilder('category')
+        .where('category.id = :id', { id: id })
+        .update(UpdateCategoryDto)
+        .execute()
+        .then((result) => {
+          return result;
+        });
+
+      return category;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    try {
+      const category = this.categoryRepository
+        .createQueryBuilder('category')
+        .where('category.id = :id', { id: id })
+        .delete()
+        .execute();
+
+      return category;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
