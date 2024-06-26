e4429c7a4242b43e53d3d260c8da33b6bd250580
tkimanh <tran.kimanh@orange.fr>
2024-06-26 19:04:38 +0200

    Modification du nom de la classe

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('category')
-export class UserEntity {
+export class CategoryEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
