65b311a9c85eb5d310920fd72a33b65d32c2dd85
tkimanh <tran.kimanh@orange.fr>
2024-06-28 12:12:20 +0200

    modification des cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 8000;
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+  });
   await app.listen(PORT);
 }
+
 bootstrap();
